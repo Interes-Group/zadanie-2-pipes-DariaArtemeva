6d4fe1a164c92af3ef68b803d063556c7665ed44
DariaArtemeva <xartemeva@gmail.com>
2023-04-23 15:45:47 +0200

    almost done v2

--- src/main/java/sk/stuba/fei/uim/oop/board/GameBoard.java
+++ src/main/java/sk/stuba/fei/uim/oop/board/GameBoard.java
@@ -19,7 +19,6 @@ public class GameBoard extends JPanel implements MouseListener, MouseMotionListe
     private List<Point> generatedPath;
     private int correctPathLen;
     private Point mousePosition;
-
     private PipeGame pipeGame;
 
     public GameBoard(PipeGame pipeGame) {
@@ -29,17 +28,158 @@ public class GameBoard extends JPanel implements MouseListener, MouseMotionListe
         pipes = new Pipe[boardSize][boardSize];
         initializeBoard();
         setPreferredSize(new Dimension(400, 400));
-
         setBackground(Color.WHITE);
-
         addMouseListener(this);
         addMouseMotionListener(this);
     }
-    public int getCurrentLevel() {
-        return currentLevel;
+
+
+    public void resetGame() {
+        initializeBoard();
+        repaint();
+    }
+
+    public void checkPath() {
+        correctPathLen = getCorrectPathLen();
+        recheckCorrectPath();
+        Pipe lastCorrectPipe = pipes[generatedPath.get(correctPathLen - 1).y][generatedPath.get(correctPathLen - 1).x];
+        Pipe endPointPipe = pipes[endPoint.y][endPoint.x];
+
+        int lastCorrectPipeOut = (lastCorrectPipe.getRotation() + 180) % 360;
+        int endPointPipeOut = (endPointPipe.getRotation() + 180) % 360;
+
+        if (lastCorrectPipe.getPoint().equals(endPoint)) {
+            newLevel();
+        } else if (Math.abs(lastCorrectPipeOut - endPointPipeOut) == 180) {
+            int dx = endPoint.x - lastCorrectPipe.getPoint().x;
+            int dy = endPoint.y - lastCorrectPipe.getPoint().y;
+            if (lastCorrectPipeOut == 0 && dx == 1 && dy == 0) {
+                newLevel();
+            } else if (lastCorrectPipeOut == 180 && dx == -1 && dy == 0) {
+                newLevel();
+            } else if (lastCorrectPipeOut == 90 && dx == 0 && dy == 1) {
+                newLevel();
+            } else if (lastCorrectPipeOut == 270 && dx == 0 && dy == -1) {
+                newLevel();
+            }
+        }
+
+        repaint();
+    }
+
+    public void newLevel() {
+        currentLevel++;
+        resetGame();
+        pipeGame.updateLevelLabel(currentLevel);
+    }
+
+    public void setBoardSize(int newSize) {
+        boardSize = newSize;
+        pipes = new Pipe[boardSize][boardSize];
+        resetGame();
+    }
+
+    public int getCorrectPathLen() {
+        int i;
+        for(i = 0; i < generatedPath.size()-1; i++) {
+            Point current = generatedPath.get(i);
+            Point next = generatedPath.get(i + 1);
+            if(!checkConnection(current, next)) break;
+        }
+        return i+1;
+    }
+
+    public void recursiveCheck(Pipe curr_pipe, Pipe prev_pipe) {
+        Point[] augment = new Point[] {
+                new Point(1, 0),
+                new Point(-1, 0),
+                new Point(0, 1),
+                new Point(0, -1)};
+        Point curr_point = curr_pipe.getPoint();
+        for(Point a: augment) {
+            Pipe neighbour = getNeighbourPipe(curr_point, a);
+            if(neighbour == null) continue;
+            if(neighbour == prev_pipe) continue;
+            if(!checkConnection(curr_point, neighbour.getPoint())) continue;
+            neighbour.setCorrect(true);
+            recursiveCheck(neighbour, curr_pipe);
+        }
+    }
+
+    private void resetCorrectPath() {
+        for(Point p: generatedPath) {
+            Pipe pipe = getPipe(p);
+            if(pipe != null) pipe.setCorrect(false);
+        }
+    }
+
+    public void recheckCorrectPath() {
+        resetCorrectPath();
+        Pipe first_pipe = getPipe(generatedPath.get(0));
+        if(first_pipe == null) return;
+        first_pipe.setCorrect(true);
+        recursiveCheck(first_pipe, null);
     }
 
 
+
+
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        int tileSize = Math.min(getWidth() / boardSize, getHeight() / boardSize);
+        drawMouseRect(g, tileSize);
+        drawBoundaryPosition(g, startPoint, tileSize, Color.RED);
+        drawBoundaryPosition(g, endPoint, tileSize, Color.GREEN);
+
+        for (Point point : generatedPath) {
+            Pipe pipe = pipes[point.y][point.x];
+            Color color = pipe.getCorrect() ? Color.BLUE : Color.BLACK;
+            pipe.draw(g, tileSize, color);
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        int tileSize = Math.min(getWidth() / boardSize, getHeight() / boardSize);
+        int x = e.getX() / tileSize;
+        int y = e.getY() / tileSize;
+
+        if (x >= 0 && x < boardSize && y >= 0 && y < boardSize) {
+            Pipe clickedPipe = pipes[y][x];
+            if (clickedPipe != null) {
+                clickedPipe.rotate();
+                correctPathLen = 0;
+                repaint();
+            }
+        }
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        mousePosition = null;
+        repaint();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        mousePosition = e.getPoint();
+        repaint();
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {}
+
+    @Override
+    public void mousePressed(MouseEvent e) {}
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
     private void initializeBoard() {
         correctPathLen = 0;
         startPoint = new Point(0, (int) (Math.random() * boardSize));
@@ -95,73 +235,25 @@ public class GameBoard extends JPanel implements MouseListener, MouseMotionListe
         int endPointRotation = 0;
         pipes[endPoint.y][endPoint.x] = new StraightPipe(endPoint.x, endPoint.y, endPointRotation);
     }
-
-    public void resetGame() {
-        initializeBoard();
-        repaint();
+    protected void drawMouseRect(Graphics g, int size) {
+        if(mousePosition == null) return;
+        g.setColor(Color.ORANGE);
+        int x = mousePosition.x / size;
+        int y = mousePosition.y / size;
+        g.fillRect(x * size, y * size, size, size);
     }
-
-    public void checkPath() {
-        correctPathLen = getCorrectPathLen();
-        recheckCorrectPath();
-        Pipe lastCorrectPipe = pipes[generatedPath.get(correctPathLen - 1).y][generatedPath.get(correctPathLen - 1).x];
-        Pipe endPointPipe = pipes[endPoint.y][endPoint.x];
-
-        int lastCorrectPipeOut = (lastCorrectPipe.getRotation() + 180) % 360;
-        int endPointPipeOut = (endPointPipe.getRotation() + 180) % 360;
-
-        if (lastCorrectPipe.getPoint().equals(endPoint)) {
-            newLevel();
-        } else if (Math.abs(lastCorrectPipeOut - endPointPipeOut) == 180) {
-            int dx = endPoint.x - lastCorrectPipe.getPoint().x;
-            int dy = endPoint.y - lastCorrectPipe.getPoint().y;
-            if (lastCorrectPipeOut == 0 && dx == 1 && dy == 0) {
-                newLevel();
-            } else if (lastCorrectPipeOut == 180 && dx == -1 && dy == 0) {
-                newLevel();
-            } else if (lastCorrectPipeOut == 90 && dx == 0 && dy == 1) {
-                newLevel();
-            } else if (lastCorrectPipeOut == 270 && dx == 0 && dy == -1) {
-                newLevel();
-            }
-        }
-
-        repaint();
+    protected void drawBoundaryPosition(Graphics g, Point point, int size, Color color) {
+        g.setColor(color);
+        g.fillOval(point.x * size, point.y * size, size, size);
     }
-
-
-
-
-
-
-    public void newLevel() {
-        currentLevel++;
-        resetGame();
-        pipeGame.updateLevelLabel(currentLevel);
-    }
-
-
-
-    public void setBoardSize(int newSize) {
-        boardSize = newSize;
-        pipes = new Pipe[boardSize][boardSize];
-        resetGame();
-    }
-
-    private Point sumPoint(Point a, Point b) {
-        return new Point(a.x + b.x, a.y + b.y);
-    }
-
     private Pipe getPipe(Point p) {
         if(p.x < 0 || p.y < 0 || p.x >= boardSize || p.y >= boardSize) return null;
         return pipes[p.y][p.x];
     }
-
     private Pipe getNeighbourPipe(Point p, Point d) {
         Point new_p = new Point(p.x + d.x, p.y + d.y);
         return getPipe(new_p);
     }
-
     private boolean checkConnection(Point p1, Point p2) {
         Pipe pipe1 = pipes[p1.y][p1.x];
         Pipe pipe2 = pipes[p2.y][p2.x];
@@ -173,118 +265,4 @@ public class GameBoard extends JPanel implements MouseListener, MouseMotionListe
 
         return pipe1.isConnectedTo(p2) && pipe2.isConnectedTo(p1);
     }
-
-
-    public int getCorrectPathLen() {
-        int i;
-        for(i = 0; i < generatedPath.size()-1; i++) {
-            Point current = generatedPath.get(i);
-            Point next = generatedPath.get(i + 1);
-            if(!checkConnection(current, next)) break;
-        }
-        return i+1;
-    }
-
-    public void recursiveCheck(Pipe curr_pipe, Pipe prev_pipe) {
-        Point[] augment = new Point[] {
-                new Point(1, 0),
-                new Point(-1, 0),
-                new Point(0, 1),
-                new Point(0, -1)};
-        Point curr_point = curr_pipe.getPoint();
-        for(Point a: augment) {
-            Pipe neighbour = getNeighbourPipe(curr_point, a);
-            if(neighbour == null) continue;
-            if(neighbour == prev_pipe) continue;
-            if(!checkConnection(curr_point, neighbour.getPoint())) continue;
-            neighbour.setCorrect(true);
-            recursiveCheck(neighbour, curr_pipe);
-        }
-    }
-
-    private void resetCorrectPath() {
-        for(Point p: generatedPath) {
-            Pipe pipe = getPipe(p);
-            if(pipe != null) pipe.setCorrect(false);
-        }
-    }
-
-    public void recheckCorrectPath() {
-        resetCorrectPath();
-        Pipe first_pipe = getPipe(generatedPath.get(0));
-        if(first_pipe == null) return;
-        first_pipe.setCorrect(true);
-        recursiveCheck(first_pipe, null);
-    }
-
-    protected void drawMouseRect(Graphics g, int size) {
-        if(mousePosition == null) return;
-        g.setColor(Color.ORANGE);
-        int x = mousePosition.x / size;
-        int y = mousePosition.y / size;
-        g.fillRect(x * size, y * size, size, size);
-    }
-
-    protected void drawBoundaryPosition(Graphics g, Point point, int size, Color color) {
-        g.setColor(color);
-        g.fillOval(point.x * size, point.y * size, size, size);
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        int tileSize = Math.min(getWidth() / boardSize, getHeight() / boardSize);
-        int counter = 0;
-
-        drawMouseRect(g, tileSize);
-        drawBoundaryPosition(g, startPoint, tileSize, Color.RED);
-        drawBoundaryPosition(g, endPoint, tileSize, Color.GREEN);
-
-        for (Point point : generatedPath) {
-            Pipe pipe = pipes[point.y][point.x];
-            Color color = pipe.getCorrect() ? Color.BLUE : Color.BLACK;
-            pipe.draw(g, tileSize, color);
-        }
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        int tileSize = Math.min(getWidth() / boardSize, getHeight() / boardSize);
-        int x = e.getX() / tileSize;
-        int y = e.getY() / tileSize;
-
-        if (x >= 0 && x < boardSize && y >= 0 && y < boardSize) {
-            Pipe clickedPipe = pipes[y][x];
-            if (clickedPipe != null) {
-                clickedPipe.rotate();
-                correctPathLen = 0;
-                repaint();
-            }
-        }
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        mousePosition = null;
-        repaint();
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent e) {
-        mousePosition = e.getPoint();
-        repaint();
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent e) {}
-
-    @Override
-    public void mousePressed(MouseEvent e) {}
-
-    @Override
-    public void mouseReleased(MouseEvent e) {}
-
-    @Override
-    public void mouseEntered(MouseEvent e) {}
 }
--- src/main/java/sk/stuba/fei/uim/oop/pipe/Pipe.java
+++ src/main/java/sk/stuba/fei/uim/oop/pipe/Pipe.java
@@ -9,7 +9,7 @@ public abstract class Pipe {
     protected int y;
     protected int rotation;
     protected boolean correct;
-    private boolean locked = false;
+
 
     public Pipe(int x, int y, int rotation) {
         this.x = x;
@@ -27,30 +27,19 @@ public abstract class Pipe {
 
     public Point getPoint() { return new Point(x, y); }
 
-    public boolean isLocked() {
-        return locked;
-    }
-
-    public void setLocked(boolean locked) {
-        this.locked = locked;
-    }
-
     public int getRotation() {
         return rotation;
     }
 
-    public void setRotation(int rotation) {
-        this.rotation = rotation;
-    }
 
     public boolean getCorrect() {return correct; }
 
     public void setCorrect(boolean correct) { this.correct = correct; }
 
     public void rotate() {
-        if (!locked) {
+
             rotation = (rotation + 90) % 360;
-        }
+
     }
 
 
